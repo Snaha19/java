fe861097432a111bfede6b08b6eed345bcd75a6f
Snaha19 <snahakundu19@gmail.com>
2026-04-15 19:42:16 +0530

    what you changed

--- sum_prime.java
+++ sum_prime.java
@@ -1,24 +1,36 @@
+import java.util.Scanner;
+
 public class sum_prime {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n :");
+        int n=sc.nextInt();
+        demo obj=new demo();
+        obj.check(n);
+
+         
     }
 }
 class demo{
-    void isprime(int n){
-            int count=0;
-            boolean isprime=false;
+    void check(int n){
+        for(int i=1;i<=n/2;i++){
+            if(isprime(i) && isprime(n-i)){
+                System.out.println(n+" = "+i+" + "+(n-i));
+            }
+        }
+            
+        
+    }
+
+    boolean isprime(int n){
             for(int i=2;i<=n/2;i++){
                 if(n%i==0){
-                    count++;
-                    break;
+                    return false;
+                    
                 }   
             }
-            if (count==0){
-                isprime=true;
-            }
-            else{
-                isprime=false;
-            }
+            return true;
+
     }
 
 }
